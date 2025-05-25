d67b473a69d753e482a867e639f343dae189b201
budiwahyu.adita <budiwahyuherlenadita@gmail.com>
2025-05-25 12:11:34 +0700

    add book and author

--- src/main/java/com/srinproject/srinlibrary/service/BookService.java
+++ src/main/java/com/srinproject/srinlibrary/service/BookService.java
@@ -3,7 +3,9 @@ package com.srinproject.srinlibrary.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.srinproject.srinlibrary.persistence.Author;
 import com.srinproject.srinlibrary.persistence.Book;
+import com.srinproject.srinlibrary.repository.AuthorRepository;
 import com.srinproject.srinlibrary.repository.BookRepository;
 
 import java.util.List;
@@ -13,11 +15,20 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
-
+    
     public Book addBook(Book book) {
+        // Check if the author exists
+        if (book.getAuthor() != null && book.getAuthor().getId() != null) {
+            Author author = authorRepository.findById(book.getAuthor().getId())
+                    .orElseThrow(() -> new RuntimeException("Author not found with id " + book.getAuthor().getId()));
+            book.setAuthor(author);
+        }
         return bookRepository.save(book);
     }
 
