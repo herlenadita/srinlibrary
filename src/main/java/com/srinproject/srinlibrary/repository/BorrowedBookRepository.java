4b9f73f9848ebf6444289eb7f7a4bcc164e2487c
budiwahyu.adita <budiwahyuherlenadita@gmail.com>
2025-05-29 13:28:55 +0700

    implement borrowed book

--- src/main/java/com/srinproject/srinlibrary/repository/BorrowedBookRepository.java
+++ src/main/java/com/srinproject/srinlibrary/repository/BorrowedBookRepository.java
@@ -4,10 +4,21 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.srinproject.srinlibrary.persistence.BorrowedBook;
 
 public interface BorrowedBookRepository extends JpaRepository<BorrowedBook, Long> {
-    List<BorrowedBook> findByBookTitleContainingOrMemberNameContainingOrBorrowDate(Date borrowDate, String bookTitle, String memberName);
+    @Query("SELECT bb FROM BorrowedBook bb " +
+           "JOIN bb.book b " +
+           "JOIN bb.member m " +
+           "WHERE (:bookTitle IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :bookTitle, '%'))) " +
+           "AND (:memberName IS NULL OR LOWER(m.name) LIKE LOWER(CONCAT('%', :memberName, '%'))) " +
+           "AND (:borrowDate IS NULL OR bb.borrowDate = :borrowDate)")
+    List<BorrowedBook> findByFilters(
+            @Param("bookTitle") String bookTitle,
+            @Param("memberName") String memberName,
+            @Param("borrowDate") Date borrowDate);
 }
 
